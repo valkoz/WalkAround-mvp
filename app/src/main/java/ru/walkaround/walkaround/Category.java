7affa665216714e0ae90d8a131d378e6deca1837
valkoz <kozlov.valent@gmail.com>
2018-04-22 14:01:28 +0300

    Add categories

---
+++ app/src/main/java/ru/walkaround/walkaround/Category.java
@@ -0,0 +1,38 @@
+package ru.walkaround.walkaround;
+
+public class Category {
+
+    private String name;
+
+    private boolean selected;
+
+    private int imageId;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public int getImage() {
+        return imageId;
+    }
+
+    public void setImage(int id) {
+        this.imageId = id;
+    }
+}
--- app/src/main/java/ru/walkaround/walkaround/CategoryActivity.java
+++ app/src/main/java/ru/walkaround/walkaround/CategoryActivity.java
@@ -6,13 +6,34 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    private final List<String> categoryNames = Arrays.asList("Museum", "Park", "Bar", "Sport");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        ListView listView = findViewById(R.id.categories_list);
+
+        List<Category> categories = new ArrayList<>();
+
+        for (String s: categoryNames) {
+            Category c = new Category(s);
+            c.setImage(getResources().getIdentifier(s.toLowerCase(), "drawable", getPackageName()));
+            categories.add(c);
+        }
+        CategoryArrayAdapter adapter = new CategoryArrayAdapter(this, categories);
+
+        listView.setAdapter(adapter);
 
     }
 }
