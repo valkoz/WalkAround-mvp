bf63db40e14524c2ba6633cd5496a3b5ee0b47c3
valkoz <kozlov.valent@gmail.com>
2018-05-20 15:52:33 +0300

    Fix keyboard first show

--- app/src/main/java/ru/walkaround/walkaround/fragments/ChooseCityFragment.java
+++ app/src/main/java/ru/walkaround/walkaround/fragments/ChooseCityFragment.java
@@ -1,5 +1,6 @@
 package ru.walkaround.walkaround.fragments;
 
+import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
@@ -93,6 +95,12 @@ public class ChooseCityFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        showKeyboard(getContext());
+    }
+
     private void onNextButtonClick() {
 
         Location userLocation = new Location("userLocation");
@@ -147,7 +155,7 @@ public class ChooseCityFragment extends Fragment {
 
                 Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
 
-                hideKeyboard();
+                hideKeyboard(getContext());
             }
         }
     };
@@ -176,15 +184,18 @@ public class ChooseCityFragment extends Fragment {
         }
     };
 
-    public void hideKeyboard() {
-        View view = getActivity().getCurrentFocus();
-        if (view != null) {
-            InputMethodManager inputManager =
-                    (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-            if (inputManager != null) {
-                inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    public void hideKeyboard(Context context) {
+        try {
+            ((Activity) context).getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+            if ((((Activity) context).getCurrentFocus() != null) && (((Activity) context).getCurrentFocus().getWindowToken() != null)) {
+                ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(((Activity) context).getCurrentFocus().getWindowToken(), 0);
             }
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void showKeyboard(Context context) {
+        ((InputMethodManager) (context).getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 }
